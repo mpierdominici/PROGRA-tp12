ebd82665cdb37122551872efafaa79683673d2ff
mpierdominici <mpierdominici@itba.edu.ar>
2016-11-12 19:37:26 -0300

    terminado funcionando/falta comentar

--- main.c
+++ main.c
@@ -4,35 +4,43 @@
 #include "teclado.h"
 #include <time.h>
 #define TAMANIO_BUFFER 10
+#define ESC 0X1b
 int main (void)
 
 {
 	pthread_t t1;
+	uint8_t carac;
+	uint8_t exit=1;
 	M_C_BUFFER eventos_teclado;
 	setsize_c_buffer(&eventos_teclado,TAMANIO_BUFFER);
 	create_c_buffer(&eventos_teclado);
 	set_buffer_teclado(&eventos_teclado);
 	pthread_create(&t1,NULL,start_teclado,NULL);
 
-	while(1)
+	while(exit)
 	{
 		
 		if (status_c_buffer(&eventos_teclado)==-1)
 		{
-
+			printf("\n");
 			while(status_c_buffer(&eventos_teclado))
 			{	
-				
-				printf("%c",pull_c_buffer(&eventos_teclado) );
+				carac=pull_c_buffer(&eventos_teclado);
+				if (carac==ESC)
+				{
+					exit=0;
+					carac=0;
+				}
+				printf("%c",carac);
 				
 			}
-			
+			printf("\n");
 		}
 
 	}
 
 
-
+	stop_teclado();
 	destroy_c_buffer(&eventos_teclado);
 	return 0;
 }
